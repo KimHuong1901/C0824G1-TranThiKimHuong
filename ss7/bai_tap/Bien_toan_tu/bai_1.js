2b9fb515d3ccb233e2d6d2ed79b1a3f524efdfee
KimHuong1901 <trankimhuong1901@gmail.com>
2024-09-11 22:42:57 +0700

    bai_tap

---
+++ ss7/bai_tap/Bien_toan_tu/bai_1.js
@@ -0,0 +1,8 @@
+let a = + prompt();
+let b = +prompt();
+let c = +prompt();
+let tong = a + b + c;
+let trungBinh = tong/3;
+document.write('Sum 3 subjects: ' + tong);
+document.write("</br>");
+document.write('Average 3 subjects: ' + trungBinh);
