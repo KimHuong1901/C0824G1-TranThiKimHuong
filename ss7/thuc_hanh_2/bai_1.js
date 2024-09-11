99c99e84a8733d3a046c9b90dbb89be47740c261
KimHuong1901 <trankimhuong1901@gmail.com>
2024-09-11 15:32:16 +0700

    thuc_hanh_2

---
+++ ss7/thuc_hanh_2/bai_1.js
@@ -0,0 +1,6 @@
+inputWidth = prompt();
+inputHeight = prompt();
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let s = width * height;
+document.write("Dien tich hinh chu nhat la: " + s);
